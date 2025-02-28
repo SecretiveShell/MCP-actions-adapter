8b445baac372d4135ee6f6c6b856c08d8e4829cb
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-02-28 22:14:46 +0000

    add config

--- src/mcp_actions_adapter/main.py
+++ src/mcp_actions_adapter/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from mcp_actions_adapter.config import config
 
 app = FastAPI(
     title="MCP Actions Adapter",
     description="A simple adapter to convert a MCP server to a GPT actions compatible API",
+    servers=[{"production": config.url}],
 )
 
